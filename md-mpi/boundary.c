ba639255b3512b86d1bd953f5e42ff64d9bd1059
Sal <sa1749@york.ac.uk>
2024-01-22 07:46:23 +0000

    mpi - dire straits

--- md-mpi/boundary.c
+++ md-mpi/boundary.c
@@ -22,68 +22,63 @@ void apply_boundary() {
 	MPI_Allgather(particles.x, num_particles_per_proc, MPI_DOUBLE, temp_part_x, num_particles_per_proc, MPI_DOUBLE, MPI_COMM_WORLD);
 	MPI_Allgather(particles.y, num_particles_per_proc, MPI_DOUBLE, temp_part_y, num_particles_per_proc, MPI_DOUBLE, MPI_COMM_WORLD);
 	
-	for (int i = 0; i < sizei+2; i++) {
-		for (int j = 0; j < sizej+2; j++) {
-			cell_count_flat[i*(sizej+2) + j] = cells[i][j].count;
-			for (int k = 0; k < cells[i][j].count; k++) {
-				cell_part_ids_flat[(i*(2*num_part_per_dim*num_part_per_dim)*(sizej+2)) + (j*2*num_part_per_dim*num_part_per_dim) + k] = cells[i][j].part_ids[k];
-			}
-		}
+	// for (int i = 0; i < sizei+2; i++) {
+	// 	for (int j = 0; j < sizej+2; j++) {
+	// 		cell_count_flat[i*(sizej+2) + j] = cells[i][j].count;
+	// 		for (int k = 0; k < cells[i][j].count; k++) {
+	// 			cell_part_ids_flat[(i*(2*num_part_per_dim*num_part_per_dim)*(sizej+2)) + (j*2*num_part_per_dim*num_part_per_dim) + k] = cells[i][j].part_ids[k];
+	// 		}
+	// 	}
+	// }
+
+	for (int j = 1; j < y+1; j++) {
+		MPI_Sendrecv(&(cells[x][j].part_ids[0]), 2*num_part_per_dim*num_part_per_dim, MPI_INT, east_rank, 1, &(cells[0][j].part_ids[0]), 2*num_part_per_dim*num_part_per_dim, MPI_INT, west_rank, 1, cart_comm, MPI_STATUS_IGNORE);
+		MPI_Sendrecv(&(cells[x][j].count), 1, MPI_INT, east_rank, 1, &(cells[0][j].count), 1, MPI_INT, west_rank, 1, cart_comm, MPI_STATUS_IGNORE);
+		
+		MPI_Sendrecv(&(cells[1][j].part_ids[0]), 2*num_part_per_dim*num_part_per_dim, MPI_INT, west_rank, 1, &(cells[1][j].part_ids[0]), 2*num_part_per_dim*num_part_per_dim, MPI_INT, east_rank, 1, cart_comm, MPI_STATUS_IGNORE);
+		MPI_Sendrecv(&(cells[1][j].count), 1, MPI_INT, west_rank, 1, &(cells[1][j].count), 1, MPI_INT, east_rank, 1, cart_comm, MPI_STATUS_IGNORE);
 	}
 
-	// for (int j = 1; j < y+1; j++) {
-	// 	MPI_Sendrecv(&(cells[x][j].part_ids[0]), 2*num_part_per_dim*num_part_per_dim, MPI_INT, east_rank, 1, &(cells[0][j].part_ids[0]), 2*num_part_per_dim*num_part_per_dim, MPI_INT, west_rank, 1, cart_comm, MPI_STATUS_IGNORE);
-	// 	cells[0][j].part_ids = cells[x][j].part_ids;
-	// 	cells[0][j].count = cells[x][j].count;
-	// 	cells[0][j].size = cells[x][j].size;
+	for (int i = 0; i < x+2; i++) {
+		MPI_Sendrecv(&(cells[i][y].part_ids[0]), 2*num_part_per_dim*num_part_per_dim, MPI_INT, south_rank, 1, &(cells[i][0].part_ids[0]), 2*num_part_per_dim*num_part_per_dim, MPI_INT, north_rank, 1, cart_comm, MPI_STATUS_IGNORE);
+		MPI_Sendrecv(&(cells[i][y].part_ids[0]), 1, MPI_INT, south_rank, 1, &(cells[i][0].count), 1, MPI_INT, north_rank, 1, cart_comm, MPI_STATUS_IGNORE);
 
-	// 	cells[x+1][j].part_ids = cells[1][j].part_ids;
-	// 	cells[x+1][j].count = cells[1][j].count;
-	// 	cells[x+1][j].size = cells[1][j].size;
-	// }
-
-	// for (int i = 0; i < x+2; i++) {
-	// 	cells[i][0].part_ids = cells[i][y].part_ids;
-	// 	cells[i][0].count = cells[i][y].count;
-	// 	cells[i][0].size = cells[i][y].size;
-
-	// 	cells[i][y+1].part_ids = cells[i][1].part_ids;
-	// 	cells[i][y+1].count = cells[i][1].count;
-	// 	cells[i][y+1].size = cells[i][1].size;
-	// }
+		MPI_Sendrecv(&(cells[i][1].part_ids[0]), 2*num_part_per_dim*num_part_per_dim, MPI_INT, north_rank, 1, &(cells[i][y+1].part_ids[0]), 2*num_part_per_dim*num_part_per_dim, MPI_INT, south_rank, 1, cart_comm, MPI_STATUS_IGNORE);
+		MPI_Sendrecv(&(cells[i][1].count), 1, MPI_INT, north_rank, 1, &(cells[i][y+1].count), 1, MPI_INT, south_rank, 1, cart_comm, MPI_STATUS_IGNORE);
+	}
 	
-	MPI_Sendrecv(&(cells[0][sizei].part_ids), (sizei+2) * 2 * num_part_per_dim * num_part_per_dim, MPI_INT, north_rank, 1, &(cells[0][0].part_ids), (sizei+2) * 2 * num_part_per_dim * num_part_per_dim, MPI_INT, south_rank, 1, cart_comm, MPI_STATUS_IGNORE);
-	MPI_Sendrecv(&(cells[0][sizei].count), (sizei+2), MPI_INT, north_rank, 1, &(cells[0][0].count), (sizei+2), MPI_INT, south_rank, 1, cart_comm, MPI_STATUS_IGNORE);
+	// MPI_Sendrecv(&(cells[0][sizei].part_ids), (sizei+2) * 2 * num_part_per_dim * num_part_per_dim, MPI_INT, north_rank, 1, &(cells[0][0].part_ids), (sizei+2) * 2 * num_part_per_dim * num_part_per_dim, MPI_INT, south_rank, 1, cart_comm, MPI_STATUS_IGNORE);
+	// MPI_Sendrecv(&(cells[0][sizei].count), (sizei+2), MPI_INT, north_rank, 1, &(cells[0][0].count), (sizei+2), MPI_INT, south_rank, 1, cart_comm, MPI_STATUS_IGNORE);
 	// MPI_Sendrecv(&(cells[1][0]).size, sizei, MPI_INT, north_rank, 1, &(cells[sizej+1][0]).size, sizei, MPI_INT, south_rank, 1, cart_comm, MPI_STATUS_IGNORE);
 
-	MPI_Sendrecv(&(cells[0][1].part_ids), (sizei+2) * 2 * num_part_per_dim * num_part_per_dim, MPI_INT, south_rank, 1, &(cells[0][sizei+1].part_ids), (sizei+2) * 2 * num_part_per_dim * num_part_per_dim, MPI_INT, north_rank, 1, cart_comm, MPI_STATUS_IGNORE);
-	MPI_Sendrecv(&(cells[0][1].count), (sizei+2), MPI_INT, south_rank, 1, &(cells[0][sizei+1].count), (sizei+2), MPI_INT, north_rank, 1, cart_comm, MPI_STATUS_IGNORE);
+	// MPI_Sendrecv(&(cells[0][1].part_ids), (sizei+2) * 2 * num_part_per_dim * num_part_per_dim, MPI_INT, south_rank, 1, &(cells[0][sizei+1].part_ids), (sizei+2) * 2 * num_part_per_dim * num_part_per_dim, MPI_INT, north_rank, 1, cart_comm, MPI_STATUS_IGNORE);
+	// MPI_Sendrecv(&(cells[0][1].count), (sizei+2), MPI_INT, south_rank, 1, &(cells[0][sizei+1].count), (sizei+2), MPI_INT, north_rank, 1, cart_comm, MPI_STATUS_IGNORE);
 
 	
 
 
 	// Apply boundary conditions
 	// exchange column 1 with right-most ghost column
-	MPI_Sendrecv(&(cell_part_ids_flat[sizej*(2*num_part_per_dim*num_part_per_dim)*(sizej+2) + (1*2*num_part_per_dim*num_part_per_dim) + 0]), sizej*2*num_part_per_dim*num_part_per_dim, MPI_INT, east_rank, 0, &(cell_part_ids_flat[0*(2*num_part_per_dim*num_part_per_dim)*(sizej+2) + (1*2*num_part_per_dim*num_part_per_dim) + 0]), sizej*2*num_part_per_dim*num_part_per_dim, MPI_INT, west_rank, 0, cart_comm, MPI_STATUS_IGNORE);
-	MPI_Sendrecv(&(cell_count_flat[sizej*(sizej+2) + 1]), sizej, MPI_INT, east_rank, 0, &(cell_count_flat[0*(sizej+2) + 1]), sizej, MPI_INT, west_rank, 0, cart_comm, MPI_STATUS_IGNORE);
+	// MPI_Sendrecv(&(cell_part_ids_flat[sizej*(2*num_part_per_dim*num_part_per_dim)*(sizej+2) + (1*2*num_part_per_dim*num_part_per_dim) + 0]), sizej*2*num_part_per_dim*num_part_per_dim, MPI_INT, east_rank, 0, &(cell_part_ids_flat[0*(2*num_part_per_dim*num_part_per_dim)*(sizej+2) + (1*2*num_part_per_dim*num_part_per_dim) + 0]), sizej*2*num_part_per_dim*num_part_per_dim, MPI_INT, west_rank, 0, cart_comm, MPI_STATUS_IGNORE);
+	// MPI_Sendrecv(&(cell_count_flat[sizej*(sizej+2) + 1]), sizej, MPI_INT, east_rank, 0, &(cell_count_flat[0*(sizej+2) + 1]), sizej, MPI_INT, west_rank, 0, cart_comm, MPI_STATUS_IGNORE);
 	// MPI_Sendrecv(&(cells[0][1]).size, 1, MPI_INT, west_rank, 0, &(cells[0][sizej+1]).size, 1, MPI_INT, east_rank, 0, cart_comm, MPI_STATUS_IGNORE);
 
 	// exchange right-most regular column with left-most ghost column 
-	MPI_Sendrecv(&(cell_part_ids_flat[1*(2*num_part_per_dim*num_part_per_dim)*(sizej+2) + (1*2*num_part_per_dim*num_part_per_dim) + 0]), sizej*2*num_part_per_dim*num_part_per_dim, MPI_INT, west_rank, 0, &(cell_part_ids_flat[(sizej+1)*(2*num_part_per_dim*num_part_per_dim)*(sizej+2) + (1*2*num_part_per_dim*num_part_per_dim) + 0]), sizej*2*num_part_per_dim*num_part_per_dim, MPI_INT, east_rank, 0, cart_comm, MPI_STATUS_IGNORE);
-	MPI_Sendrecv(&(cell_count_flat[1*(sizej+2) + 1]), sizej, MPI_INT, west_rank, 0,&(cell_count_flat[(sizej+1)*(sizej+2) + 1]), sizej, MPI_INT, east_rank, 0, cart_comm, MPI_STATUS_IGNORE);
+	// MPI_Sendrecv(&(cell_part_ids_flat[1*(2*num_part_per_dim*num_part_per_dim)*(sizej+2) + (1*2*num_part_per_dim*num_part_per_dim) + 0]), sizej*2*num_part_per_dim*num_part_per_dim, MPI_INT, west_rank, 0, &(cell_part_ids_flat[(sizej+1)*(2*num_part_per_dim*num_part_per_dim)*(sizej+2) + (1*2*num_part_per_dim*num_part_per_dim) + 0]), sizej*2*num_part_per_dim*num_part_per_dim, MPI_INT, east_rank, 0, cart_comm, MPI_STATUS_IGNORE);
+	// MPI_Sendrecv(&(cell_count_flat[1*(sizej+2) + 1]), sizej, MPI_INT, west_rank, 0,&(cell_count_flat[(sizej+1)*(sizej+2) + 1]), sizej, MPI_INT, east_rank, 0, cart_comm, MPI_STATUS_IGNORE);
 	// MPI_Sendrecv(&(cells[0][sizej]).size, 1, MPI_INT, west_rank, 0, &(cells[0][0]).size, 1, MPI_INT, east_rank, 0, cart_comm, MPI_STATUS_IGNORE);
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Barrier(cart_comm);
 
-	for (int i = 0; i < sizei+2; i++) {
-		for (int j = 0; j < sizej+2; j++) {
-			cells[i][j].count = cell_count_flat[i*(sizej+2) + j];
-			for (int k = 0; k < cells[i][j].count; k++) {
-				cells[i][j].part_ids[k] = cell_part_ids_flat[(i*(2*num_part_per_dim*num_part_per_dim)*(sizej+2)) + (j*2*num_part_per_dim*num_part_per_dim) + k];
-			}
-		}
-	}
+	// for (int i = 0; i < sizei+2; i++) {
+	// 	for (int j = 0; j < sizej+2; j++) {
+	// 		cells[i][j].count = cell_count_flat[i*(sizej+2) + j];
+	// 		for (int k = 0; k < cells[i][j].count; k++) {
+	// 			cells[i][j].part_ids[k] = cell_part_ids_flat[(i*(2*num_part_per_dim*num_part_per_dim)*(sizej+2)) + (j*2*num_part_per_dim*num_part_per_dim) + k];
+	// 		}
+	// 	}
+	// }
 
 	for (int p = 0; p < num_particles_total; p++) {
 		particles.ax[p] = temp_part_ax[p];
